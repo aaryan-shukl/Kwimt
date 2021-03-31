cbb45dab10b445208f4b1b7d2acfa54724685bb5
Aaryan Shukla <78203854+aaryan-shukl@users.noreply.github.com>
2021-03-31 14:13:28 +0530

    Add files via upload

--- mainpage.js
+++ mainpage.js
@@ -1,12 +1,6 @@
 function logout() {
-     localStorage.removeItem("U");
-     window.location = "login.html";
-}
-function getdata() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-    Room_names = childKey;
-   
-   });});
-    
+    localStorage.removeItem("U");
+    window.location = "login.html";
 }
 const confrig = {
     apiKey: "AIzaSyCDrWpHdG5axhyGQgowGE8s4E3OfrKsmCI",
@@ -17,3 +11,24 @@ const confrig = {
     messagingSenderId: "985927316423",
     appId: "1:985927316423:web:4e48282518ff7e048fdfe2"
 };
+var welcomename = localStorage.getItem("U");
+document.getElementById("welcome-heading").innerHTML = "Welcome" + welcomename + "!";
+function CreateRoom() {
+    var CreatedRoomName = document.getElementById("roomname").value;
+    firebase.database().ref("/").child(CreatedRoomName).update({
+        purpose: "Adding roomname in firebase"
+    });
+    localStorage.setItem("newroomname",CreatedRoomName);
+    window.location = "roompage.html";
+}
+function getdata() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {var childKey  = childSnapshot.key;
+        var RoomNames = childKey;
+        console.log(RoomNames);
+        var roomnamedisplay = "<div class='display' id='#" + RoomNames + "' onclick='addroomuser(this.id)'>#" + RoomNames + "</div><hr>";
+        document.getElementById("available-rooms").innerHTML = roomnamedisplay;
+        function addroomuser(thispara) {
+            localStorage.setItem("roomname",RoomNames);
+            window.location = "room.html";
+        }
+    });});
+}
